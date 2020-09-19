79b11a373bef66fe89189f7135b0026b7074aae2
Nils <nils@nilsand.re>
2020-09-19 18:43:46 +0100

    Use get_json() instead of json
    json is deprecated

--- onlypolls/api.py
+++ onlypolls/api.py
@@ -72,7 +72,7 @@ def get_poll(id):
 @login_required
 def create_poll():
     body = request.get_json()
-    poll = Poll(text=body["text"], multiple_answers=body["multiple_answers"])
+    poll = Poll(title=body["title"], multiple_answers=body["multiple_answers"])
     for choice in body["choices"]:
        db_choice = Choice(text=choice)
        poll.choices.append(db_choice)
