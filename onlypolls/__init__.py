13a36c3f0fa2bd707571f5390db710fc03738536
blunagi <blunagi@blunagi>
2020-09-19 10:30:05 +0000

    Separate api into different file

--- onlypolls/__init__.py
+++ onlypolls/__init__.py
@@ -3,17 +3,12 @@ import os
 from flask import Flask, Blueprint
 from flask_sqlalchemy import SQLAlchemy
 
-from onlypolls import models
+from onlypolls import models, api
 
 app = Flask(__name__)
-api_bp = Blueprint("api", __name__)
 db = SQLAlchemy()
 
 app.config["DEBUG"] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLITE_URL")
 
-@api_bp.route("/")
-def index():
-    return "Hello world"
-
-app.register_blueprint(api_bp, url_prefix="/api")
+app.register_blueprint(api.api_bp, url_prefix="/api")
