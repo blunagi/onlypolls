7117ab00d148ca4cb158ce4738a8e5e6485d173c
Uzay-G <uzgirit@gmail.com>
2020-09-19 15:21:16 +0200

    BEGIN implementing get_polls

--- onlypolls/api.py
+++ onlypolls/api.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from onlypolls.models import Poll, User, Choice
+
 from onlypolls import db
 
 api_bp = Blueprint("api", __name__)
@@ -6,4 +8,20 @@ api_bp = Blueprint("api", __name__)
 @api_bp.route("/")
 def index():
     return "Hello world"
+
+@api_bp.route("/get_polls")
+def get_polls():
+    polls = Poll.query.all()
+
+    serialized_polls = []
+
+    for poll in polls:
+        cur_poll = {
+            "title": poll.title,
+            "choices": poll.choices,
+            "id": poll.id
+        } 
+        serialized_polls.append(cur_poll)
+
+    return jsonify(serialized_polls)
 
