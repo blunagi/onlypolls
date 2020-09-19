8e2c576417297c676e42c3cbdfea43fc88c69c25
Uzay-G <uzgirit@gmail.com>
2020-09-19 10:28:16 +0200

    api prefix setup

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 app = Flask(__name__)
+api_bp = Blueprint("api", __name__)
 
 
-@app.route("/")
+
+@api_bp.route("/")
 def index():
     return "Hello world"
+
+
+app.register_blueprint(api_bp, url_prefix="/api")
